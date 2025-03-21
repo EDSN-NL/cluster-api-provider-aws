62e0128cdea1696fa23aa2bc73caa827d3a6a663
Robin Ketelbuters <robin.ketelbuters@edsn.nl>
2025-03-21 10:41:20 +0100

    Add release script

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export RELEASE_BRANCH="release-2.8-ish"
+export PREVIOUS_VERSION="v2.7.1"
+# Token must be generated and set locally. Go to your GitHub Profile,
+# then Developer Settings, then Personal access tokens, then Fine-grained tokens.
+# Generate a new token with the "Resource owner" set to "EDSN-NL".
+# Set "Repository access" to "Only select repositories" and this repo.
+# For "Permissions", set "Contents" to "Read and write".
+# Don't commit this 🫠
+# export GITHUB_TOKEN=
+
+make release
